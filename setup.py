8f8cc8972dc14ce62d22428bb8dcac6fafb66cec
Kazim <kazimshah39@gmail.com>
2024-09-26 08:45:17 +0500

    ready

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="google_drive_id_extractor",
-    version="0.1.0",
+    version="1.2.0",
     packages=find_packages(),
     description="This will extract the Google Drive file ID from any provided Google Drive link, making it easy to retrieve the ID programmatically. It's lightweight, simple to use, and supports various Google Drive link formats.",
     long_description=open("README.md").read(),
